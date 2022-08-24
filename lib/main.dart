01e4fa6b8543397b537304b0dfef5b52e0ce16b7
michalfirszt <michalfiszu@gmail.com>
2022-08-24 15:53:57 +0200

    Screen element actions

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,33 @@ void main() {
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  static String appBarTitle = 'Posts app';
+  static List<Widget> posts = [const Text('Post1'), const Text('Post2')];
+
+  void buttonAction(int actionNumber) {
+    // ignore: avoid_print
+    print('action $actionNumber');
+  }
+
   @override
   Widget build(BuildContext context) {
-    String appBarTitle = 'Posts app';
-    List<Widget> posts = [const Text('Post1'), const Text('Post2')];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text(appBarTitle)),
         body: Column(children: [
           ...posts,
-          const ElevatedButton(onPressed: null, child: Text('Action1'))
+          ElevatedButton(
+            onPressed: () => buttonAction(1),
+            child: const Text('Action1'),
+          ),
+          ElevatedButton(
+            onPressed: () => buttonAction(2),
+            child: const Text('Action2'),
+          ),
+          ElevatedButton(
+            onPressed: () => buttonAction(3),
+            child: const Text('Action3'),
+          ),
         ]),
       ),
     );
