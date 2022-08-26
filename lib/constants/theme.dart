97a07adbcbb36182db5854587aba033b39f91e4e
michalfirszt <michalfiszu@gmail.com>
2022-08-26 20:51:27 +0200

    PostItem styles

---
+++ lib/constants/theme.dart
@@ -0,0 +1,1 @@
+double spaceUnit = 8;
--- lib/widgets/post_item.dart
+++ lib/widgets/post_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants/theme.dart' as theme;
+
 class PostItem extends StatelessWidget {
   const PostItem(
       {Key? key, required this.title, this.description = '', this.content})
@@ -9,8 +11,17 @@ class PostItem extends StatelessWidget {
   final String description;
   final String? content;
 
+  // SizedBox(width: double.infinity,)
   @override
   Widget build(BuildContext context) {
-    return Text(title);
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(theme.spaceUnit),
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: theme.spaceUnit * 3),
+      ),
+    );
   }
 }
